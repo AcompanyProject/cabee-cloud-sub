4989a2ece17683ff7372b44d86d6919babf28bdf
kMJT000 <the.daichu.2018@gmail.com>
2024-01-24 04:10:46 +0000

    返済注文時の限月pulldownチェックループ

--- selenium_chrome/source/modules/repayment_order.py
+++ selenium_chrome/source/modules/repayment_order.py
@@ -15,33 +15,13 @@ def operation_repayment_order(driver, purpose):
         time.sleep(3)
 
         retry_count = 1
-        order_kind = ''
-        order_kind2 = ''
+        order_kind, order_kind2 = None, None
+        while retry_count < 5 and order_kind is None:
+            retry_count += 1
+            order_kind, order_kind2 = get_order_kind(driver, retry_count)
 
-        order_pulldown.operation_pulldown(driver, retry_count)
-        driver.find_element_by_class_name('dealing-type-refund-futop').click() # 返済を選択
-        time.sleep(2)
-        driver.find_element_by_id('fut-op-speed-order-input-position-list-button').click() # 建玉指定ボタン押下
-        time.sleep(2)
-
-        # 建玉選択モーダル内の操作
-        if len(driver.find_elements_by_class_name('grid-body-empty')) >= 1:
-            # 買い建玉が存在しない
-            driver.find_element_by_css_selector('.switch.sell-toggle').click()
-            time.sleep(1)
-
-            if len(driver.find_elements_by_class_name('grid-body-empty')) >= 1:
-                # 売り建玉が存在しない
-                WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.common-modal-close-btn.cancel-btn'))).click() # モーダルを閉じる
-                time.sleep(2)
-            else:
-                # 売り建玉が存在する
-                order_kind = 'buy-orders'
-                order_kind2 = '.order-label.buy'
-        else:
-            # 買い建玉が存在する
-            order_kind = 'sell-orders'
-            order_kind2 = '.order-label.sell'
+        if order_kind is None:
+            raise Exception('get_order_kind returned None after 4 attempts')
 
         # 全数量選択
         driver.find_element_by_class_name('select-position-btn').click()
@@ -61,3 +41,34 @@ def operation_repayment_order(driver, purpose):
         driver.save_screenshot('log/image/error/repayment-order.png')
         slack.send_message('error', '返済注文中にエラー Error: ' + str(err))
         raise
+
+def get_order_kind(driver, retry_count):
+    order_kind = None
+    order_kind2 = None
+
+    order_pulldown.operation_pulldown(driver, retry_count)
+    driver.find_element_by_class_name('dealing-type-refund-futop').click() # 返済を選択
+    time.sleep(2)
+    driver.find_element_by_id('fut-op-speed-order-input-position-list-button').click() # 建玉指定ボタン押下
+    time.sleep(2)
+
+    # 建玉選択モーダル内の操作
+    if len(driver.find_elements_by_class_name('grid-body-empty')) >= 1:
+        # 買い建玉が存在しない
+        driver.find_element_by_css_selector('.switch.sell-toggle').click()
+        time.sleep(1)
+
+        if len(driver.find_elements_by_class_name('grid-body-empty')) >= 1:
+            # 売り建玉が存在しない
+            WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.common-modal-close-btn.cancel-btn'))).click() # モーダルを閉じる
+            time.sleep(2)
+        else:
+            # 売り建玉が存在する
+            order_kind = 'buy-orders'
+            order_kind2 = '.order-label.buy'
+    else:
+        # 買い建玉が存在する
+        order_kind = 'sell-orders'
+        order_kind2 = '.order-label.sell'
+
+    return order_kind, order_kind2
