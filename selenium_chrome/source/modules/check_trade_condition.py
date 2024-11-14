3dd84a0c75e3ca99160448e9c991d834a1fcbe98
kMJT000 <the.daichu.2018@gmail.com>
2024-11-14 15:48:36 +0000

    取引時間外の通知はなくす

--- selenium_chrome/source/modules/check_trade_condition.py
+++ selenium_chrome/source/modules/check_trade_condition.py
@@ -2,7 +2,7 @@ from log import slack
 
 def is_trader_run_condition(signal_res):
     if signal_res["trade_type"] == "none":
-        slack.send_message('notice', '取引時間外')
+        # slack.send_message('notice', '取引時間外')
         return False
     elif signal_res["is_graph_updating"] is True:
         slack.send_message('notice', 'グラフ更新作業中のため一時停止中')
